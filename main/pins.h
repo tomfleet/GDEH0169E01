b494728b185c99e266682f222675be65358a243e
tomfleet <tomfleet@gmail.com>
2026-02-11 20:44:13 +0000

    initial layout

---
+++ main/pins.h
@@ -0,0 +1,29 @@
+/*
+Adapter board Pinout
+
+    1. +3v3
+    2. GND
+    3. SDA
+    4. SCL
+    5. CS
+    6. DC
+    7. RES#
+    8. BUSY
+    9. MS
+    10. CSB2
+*/
+
+#ifndef PINS_H
+
+#define PINS_H  
+// Pin definitions
+#define PIN_SDA 3
+#define PIN_SCL 4
+#define PIN_CS 5
+#define PIN_DC 6
+#define PIN_RES 7
+#define PIN_BUSY 8
+#define PIN_MS 9
+#define PIN_CSB2 10
+
+#endif // PINS_H
